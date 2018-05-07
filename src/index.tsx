eefa0fb8c01fcc9541b899d88fef6e1232bb7eac
Omar Diab <me@omardiab.com>
2018-05-06 23:23:49 -0700

    implement simple line progress bar

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,47 @@
 import * as React from "react";
 import * as classnames from "classnames";
 
+export enum ProgressBarType {
+  LINE = "line"
+}
+
 export interface Props {
   progress: number;
   className?: string;
+  type?: ProgressBarType.LINE;
+}
+
+const CLASSNAME_DELIMITER = "--";
+
+function concatClassNames(delimiter: string, ...names: string[]): string {
+  return names.join(delimiter);
 }
 
 // IMPORTANT: Ensure this matches the classname in index.css.
-const PROGRESS_BAR_CLASSNAME = "osdiab--react-progress-bar";
+const BASE_CLASSNAME = concatClassNames(
+  CLASSNAME_DELIMITER,
+  "osdiab",
+  "react-progress-bar"
+);
+
+function genClassName(...names: string[]): string {
+  return concatClassNames(CLASSNAME_DELIMITER, BASE_CLASSNAME, ...names);
+}
 
 const ProgressBar: React.StatelessComponent<Props> = ({
   progress,
-  className
+  className,
+  type = ProgressBarType.LINE
 }) => {
+  const fullClassName = classnames([
+    BASE_CLASSNAME,
+    genClassName(`type-${type}`),
+    className
+  ]);
+  const progressClassName = classnames([genClassName("progressBar")]);
   return (
-    <div className={classnames(PROGRESS_BAR_CLASSNAME, className)}>
-      <span>{progress}</span>
+    <div className={fullClassName}>
+      <div className={progressClassName} style={{ width: `${progress}%` }} />
     </div>
   );
 };
