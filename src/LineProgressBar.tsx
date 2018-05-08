d2a52c47252ad63fb87db3d79c7997997017feb1
Omar Diab <me@omardiab.com>
2018-05-08 15:42:56 -0700

    make line progress bar more consistent in style names

--- src/LineProgressBar.tsx
+++ src/LineProgressBar.tsx
@@ -1,22 +1,19 @@
 import * as React from "react";
-import * as classnames from "classnames";
-import {
-  genClassNameWithLibraryDefaultBase,
-  genClassNameWithManyBases
-} from "./util/classNames";
-import { BaseProps, ProgressBarType, baseClassName } from ".";
+import { genClassNameWithManyBases } from "./util/classNames";
+import { BaseProps, ProgressBarType, genBaseClassName } from ".";
 
 export interface Props extends BaseProps {}
 const LineProgressBar: React.StatelessComponent<Props> = ({
   progress,
   className
 }) => {
-  const parentClassName = baseClassName(ProgressBarType.LINE, className);
+  const parentClassName = genBaseClassName(ProgressBarType.LINE, className);
   // generate appropriate class names for internal methods
-  const progressClassName = classnames(
-    genClassNameWithLibraryDefaultBase("progressBar"),
-    className && genClassNameWithManyBases(className.split(" "), "progressBar")
+  const progressClassName = genClassNameWithManyBases(
+    parentClassName.split(" "),
+    "progressBar"
   );
+
   return (
     <div className={parentClassName}>
       <div className={progressClassName} style={{ width: `${progress}%` }} />
