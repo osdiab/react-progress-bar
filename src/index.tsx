81e4f4075cd7851dfc5cf48c49b60e94d817a4a5
Omar Diab <me@omardiab.com>
2018-05-08 14:49:04 -0700

    use LineProgressBar, classname utils

--- src/index.tsx
+++ src/index.tsx
@@ -1,54 +1,43 @@
 import * as React from "react";
 import * as classnames from "classnames";
 
+import LineProgressBar from "./LineProgressBar";
+import {
+  BASE_CLASSNAME,
+  genClassNameWithLibraryDefaultBase
+} from "./util/classNames";
+
 export enum ProgressBarType {
   LINE = "line"
 }
 
-export interface Props {
+export interface BaseProps {
   progress: number;
   className?: string;
-  type?: ProgressBarType.LINE;
 }
 
-const CLASSNAME_DELIMITER = "--";
-
-function concatClassNames(delimiter: string, ...names: string[]): string {
-  return names.join(delimiter);
+export interface Props extends BaseProps {
+  type?: ProgressBarType;
 }
 
-// IMPORTANT: Ensure this matches the classname in index.css.
-const BASE_CLASSNAME = concatClassNames(
-  CLASSNAME_DELIMITER,
-  "osdiab",
-  "react-progress-bar"
-);
-
-function genClassName(...names: string[]): string {
-  return concatClassNames(CLASSNAME_DELIMITER, BASE_CLASSNAME, ...names);
-}
+export const baseClassName = (type: ProgressBarType, className?: string) =>
+  classnames(
+    BASE_CLASSNAME,
+    genClassNameWithLibraryDefaultBase(`type-${type}`),
+    className
+  );
 
 const ProgressBar: React.StatelessComponent<Props> = ({
   progress,
   className,
   type = ProgressBarType.LINE
 }) => {
-  const fullClassName = classnames(
-    BASE_CLASSNAME,
-    genClassName(`type-${type}`),
-    className
-  );
-  const progressClassName = classnames(
-    genClassName("progressBar"),
-    className
-      ? concatClassNames(CLASSNAME_DELIMITER, className, "progressBar")
-      : undefined
-  );
-  return (
-    <div className={fullClassName}>
-      <div className={progressClassName} style={{ width: `${progress}%` }} />
-    </div>
-  );
+  switch (type) {
+    default:
+      throw new Error("Invalid ProgressBar Type"); // fallthrough to LINE
+    case ProgressBarType.LINE:
+      return <LineProgressBar progress={progress} className={className} />;
+  }
 };
 
 export default ProgressBar;
